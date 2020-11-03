31848a70c1c405924da341b22983e356cecf03d3
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-11-03 23:00:55 +0000

    Update main.js

--- wwwroot/pages/main.js
+++ wwwroot/pages/main.js
@@ -7,11 +7,20 @@ $.pages.main = (() => {
 
   const main = document.createElement("main");
   
-  const button = document.createElement("button");
+  let button = document.createElement("button");
   main.appendChild(button);
   button.textContent = "Sign In";
   button.onpointerdown = () => {
     $.db.query("Sign-In", {"Username":"1", "Password":"2"}, $d => {
+      alert($d);
+    });
+  };
+  
+  button = document.createElement("button");
+  main.appendChild(button);
+  button.textContent = "Sign Out";
+  button.onpointerdown = () => {
+    $.db.query("Sign-Out", {}, $d => {
       alert($d);
     });
   };
