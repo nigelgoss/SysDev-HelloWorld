8528b65c6631ef9144960f75ca2323cdfa79cf25
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-11-03 23:19:32 +0000

    Update index.php

--- wwwroot/api/index.php
+++ wwwroot/api/index.php
@@ -2,7 +2,7 @@
 
 $input = file_get_contents('php://input');
 $input = json_decode($input, true);
-echo $input[0];
-//require_once("../../api/{$input[0]}.php");
+
+require_once("../../api/{$input[0]}.php");
 
 ?>
