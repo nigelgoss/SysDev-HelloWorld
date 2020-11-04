e13c09e1a2f2ae76e6c92c6569f67908a68e85fd
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-11-04 22:38:41 +0000

    Update main-js-config.sh

--- .circleci/main-js-config.sh
+++ .circleci/main-js-config.sh
@@ -4,8 +4,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-#cat << EOF > ~/app/wwwroot/config.js
-#(() => {
-#  \$.ENV = "PROD";
-#})();
-#EOF
+cat << EOF > ~/app/wwwroot/config.js
+(() => {
+  \$.ENV = "PROD";
+})();
+EOF
