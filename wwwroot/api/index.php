040e9c8168276c46d85ecd05683a38d370a8fb33
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-11-09 22:10:29 +0000

    Update index.php

--- wwwroot/api/index.php
+++ wwwroot/api/index.php
@@ -1,8 +1,6 @@
 <?php
 
-$input = file_get_contents('php://input');
-$input = json_decode($input, true);
-
-require_once("../../api/{$input[0]}.php");
+$dsn = "sqlsrv:host=ng.cesrz10dkvvb.eu-west-2.rds.amazonaws.com;port=1433;dbname=rdsadmin;charset=utf8";
+$pdo = new PDO($dsn, $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD']);
 
 ?>
